0a1fdfe49d7bce8e27efe42bebefa9726e3d873a
ntduong3 <nhocvipditimvo9x@gmail.com>
2022-07-18 09:23:40 -0700

    Updates

---
+++ precache-manifest.79dbcefbf2944bdc44dddba2fa65ee55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc6af51482e9bbf026bee3c78c3a8f2",
+    "url": "/app-ck-ntduong/index.html"
+  },
+  {
+    "revision": "e5334f8e78274cb8c8e9",
+    "url": "/app-ck-ntduong/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "538d93e074f65aae2f13",
+    "url": "/app-ck-ntduong/static/css/main.fdf244bd.chunk.css"
+  },
+  {
+    "revision": "e5334f8e78274cb8c8e9",
+    "url": "/app-ck-ntduong/static/js/2.6a66ec70.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/app-ck-ntduong/static/js/2.6a66ec70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538d93e074f65aae2f13",
+    "url": "/app-ck-ntduong/static/js/main.66bdf491.chunk.js"
+  },
+  {
+    "revision": "4ba3dcf18a6a7a2498af",
+    "url": "/app-ck-ntduong/static/js/runtime-main.aa290d9b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/app-ck-ntduong/static/media/logo.9f7e506d.svg"
+  }
+]);
